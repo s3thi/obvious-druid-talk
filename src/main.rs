094a280f8254fb190c21625274f25f749f8d2832
Ankur Sethi <contact@ankursethi.in>
2020-02-20 15:45:29 +0530

    Add Druid and create a simple window with a label

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use druid::widget::{Align, Label};
+use druid::{AppLauncher, Widget, WindowDesc};
+
 fn main() {
-    println!("Hello, world!");
+    let window = WindowDesc::new(ui_builder);
+    let data: u32 = 0;
+    AppLauncher::with_window(window)
+        .use_simple_logger()
+        .launch(data)
+        .expect("launch failed");
+}
+
+fn ui_builder() -> impl Widget<u32> {
+    Align::centered(Label::new("Hello, world!"))
 }

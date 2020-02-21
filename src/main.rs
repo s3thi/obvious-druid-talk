541dfcf053f3030762b3987650319596398fd4c8
Ankur Sethi <contact@ankursethi.in>
2020-02-21 14:02:33 +0530

    Add application state

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use druid::widget::{Align, Label};
-use druid::{AppLauncher, Widget, WindowDesc};
+use druid::{AppLauncher, Data, Widget, WindowDesc};
+use std::sync::Arc;
+
+struct TodoItem {
+    task: String,
+    is_completed: bool,
+}
+
+#[derive(Data, Clone)]
+struct AppState {
+    todo_list: Arc<Vec<TodoItem>>,
+    todo_entry: String,
+}
 
 fn main() {
     let window = WindowDesc::new(ui_builder);
-    let data: u32 = 0;
+    let data = AppState {
+        todo_list: Arc::new(vec![]),
+        todo_entry: "".to_string(),
+    };
     AppLauncher::with_window(window)
         .use_simple_logger()
         .launch(data)
         .expect("launch failed");
 }
 
-fn ui_builder() -> impl Widget<u32> {
+fn ui_builder() -> impl Widget<AppState> {
     Align::centered(Label::new("Hello, world!"))
 }
